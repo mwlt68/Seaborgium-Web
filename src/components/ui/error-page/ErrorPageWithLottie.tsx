34369632aadafefd2ddbb57c33c1d10cc3b678dc
mwlt68 <mwltgr@gmail.com>
2023-02-18 00:42:05 +0300

    Error page update

--- src/components/ui/error-page/ErrorPageWithLottie.tsx
+++ src/components/ui/error-page/ErrorPageWithLottie.tsx
@@ -2,27 +2,35 @@ import { Alert, Box } from "@mui/material";
 import Lottie from "lottie-react";
 import errorAnimation from "../../../assets/lotties/error.json"
 
-export function ErrorPageWithLottie(props:any){
-    const styles={
-        container:{
-            display: "flex",
-            height: "100%",
-            width: "100%",
-            flexDirection: "column",
-            alignItems:"center",
-            justifyContent: "center",
-            gap: "1rem"
-        },
-        alert:{
-            maxWidth:"50%"
-        }
+type Props = {
+    children: any;
+    message:string|null|undefined;
+}
+const styles={
+    container:{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        flexDirection: "column",
+        alignItems:"center",
+        justifyContent: "center",
+        gap: "1rem"
+    },
+    alert:{
+        maxWidth:"50%"
     }
-    return(
-        <Box sx={styles.container}>
-        <Lottie animationData={errorAnimation} loop={true}/>
-        <Alert variant="filled" severity="error" sx={styles.alert} >
-          {props.message}
-        </Alert>
-      </Box>
-    );
+}
+
+export function ErrorPageWithLottie(props:Props){
+    if(props.message != null){
+        return(
+            <Box sx={styles.container}>
+            <Lottie animationData={errorAnimation} loop={true}/>
+            <Alert variant="filled" severity="error" sx={styles.alert} >
+              {props.message}
+            </Alert>
+          </Box>
+        );
+    }
+    else return props.children
 }
